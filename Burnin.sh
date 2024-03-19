b7d98d3b1077dc237edd2b37ae57f5b3133b4b03
Lucas-Jacobsen <97192501+Lucas-Jacobsen@users.noreply.github.com>
2024-03-19 16:35:15 +0000

    fixed duration

--- Burnin.sh
+++ Burnin.sh
@@ -8,11 +8,8 @@
 echo "Enter number of CPU cores you want to stress:" 
 read cores
 
-echo "Enter timeout in seconds:" 
+echo "Enter Duration in seconds:" 
 read timeout
-
-echo "Enter interval in seconds for processor info:" 
-read interval
 
 echo "Enter file to write processor info to:" 
 read processor_file
@@ -22,7 +19,7 @@ read temp_file
 
 stress-ng -cpu $cores -timeout $timeout &
 
-sudo watch -n $interval -d 'dmidecode -t processor | grep Hz | sed "s/^/$(date) /" | tee -a $processor_file' &
+sudo watch -n 60 -d 'dmidecode -t processor | grep Hz | sed "s/^/$(date) /" | tee -a $processor_file' &
 
 sudo watch -n 60 -d 'ipmi-sensors | grep Temp | sed "s/^/$(date) /" | tee -a $temp_file' &
 
